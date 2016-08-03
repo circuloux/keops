697f4315236f487077cd97af246937b71bd0c0fb
circuloux <marioglezhdez@me.com>
2016-08-03 11:36:23 -0500

    Cambios a Index.html

--- include/quickcontact.php
+++ include/quickcontact.php
@@ -5,8 +5,8 @@ require_once('phpmailer/PHPMailerAutoload.php');
 $toemails = array();
 
 $toemails[] = array(
-				'email' => 'username@website.com', // Your Email Address
-				'name' => 'Your Name' // Your Name
+				'email' => 'tony_ventas@keopscomputers.com.mx', // Your Email Address
+				'name' => 'Tony' // Your Name
 			);
 
 // Form Processing Messages
